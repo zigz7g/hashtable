827277c62911017be899ce5303aa872223d05efb
zigz7g <rgor0204@gmail.com>
2023-12-25 02:04:10 +0300

    change task to create stack

--- hashtable/main.cpp
+++ hashtable/main.cpp
@@ -1,215 +1,270 @@
 #include <iostream>
-#include <string>
-#include <limits>
-#include "Test1.cpp"
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 
 template <typename T>
-struct HashNode {
-    int key;
-    T value;
-    HashNode* next;
-    HashNode(int k, const T& v) : key(k), value(v), next(nullptr) {}
-};
-
-
-template <typename T>
-class HashTable {
+class Stack {
 private:
-    int tableSize;  
-    HashNode<T>** table;
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* top;
 
 public:
-    HashTable();
-    explicit HashTable(int size);
-    ~HashTable();
+    Stack() : top(nullptr) {}
 
-    void printAllElements() const;
-    int getTableSize() const;
-    void insert(int key, const T& value);
-    bool remove(int key);
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+
+    void push(const T& value) {
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            throw std::out_of_range("Stack is empty");
+        }
+
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    T& peek() const {
+        if (isEmpty()) {
+            throw std::out_of_range("Stack is empty");
+        }
+
+        return top->data;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Stack& stack) {
+        Node* current = stack.top;
+        while (current != nullptr) {
+            os << current->data << " ";
+            current = current->next;
+        }
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Stack& stack) {
+        T value;
+        is >> value;
+        stack.push(value);
+        return is;
+    }
+
+    friend std::ofstream& operator<<(std::ofstream& ofs, const Stack& stack) {
+        Node* current = stack.top;
+        while (current != nullptr) {
+            ofs << current->data << " ";
+            current = current->next;
+        }
+        return ofs;
+    }
+
+    friend std::ifstream& operator>>(std::ifstream& ifs, Stack& stack) {
+        T value;
+        while (ifs >> value) {
+            stack.push(value);
+        }
+        return ifs;
+    }
+
+    friend std::istringstream& operator>>(std::istringstream& iss, Stack& stack) {
+        T value;
+        while (iss >> value) {
+            stack.push(value);
+        }
+        return iss;
+    }
+
+    friend std::ostringstream& operator<<(std::ostringstream& oss, const Stack& stack) {
+        Node* current = stack.top;
+        while (current != nullptr) {
+            oss << current->data << " ";
+            current = current->next;
+        }
+        return oss;
+    }
+
+    class Iterator {
+    private:
+        Node* current;
+
+    public:
+        Iterator(Node* start) : current(start) {}
+
+        bool operator!=(const Iterator& other) const {
+            return current != other.current;
+        }
+
+        Iterator& operator++() {
+            if (current != nullptr) {
+                current = current->next;
+            }
+            return *this;
+        }
+
+        T& operator*() const {
+            return current->data;
+        }
+    };
+
+    Iterator begin() const {
+        return Iterator(top);
+    }
+
+    Iterator end() const {
+        return Iterator(nullptr);
+    }
+};
+
+template <typename T>
+class Menu {
+public:
+    static void run() {
+        Stack<T> stack;
+
+        while (true) {
+            printMenu();
+            char choice;
+            std::cin >> choice;
+
+            switch (choice) {
+            case '1':
+                addItem(stack);
+                break;
+            case '2':
+                editItem(stack);
+                break;
+            case '3':
+                displayItems(stack);
+                break;
+            case '4':
+                deleteItem(stack);
+                break;
+            case '5':
+                saveToFile(stack);
+                break;
+            case '6':
+                loadFromFile(stack);
+                break;
+            case '7':
+                return;
+            default:
+                std::cout << "Invalid choice. Please try again." << std::endl;
+            }
+        }
+    }
 
 private:
+    static void printMenu() {
+        std::cout << "\nMenu:\n"
+            << "1. Add Item\n"
+            << "2. Edit Item\n"
+            << "3. Display Items\n"
+            << "4. Delete Item\n"
+            << "5. Save to File\n"
+            << "6. Load from File\n"
+            << "7. Exit\n"
+            << "Enter your choice: ";
+    }
 
-    int hashFunction(int key) const;
+    static void addItem(Stack<T>& stack) {
+        T value;
+        std::cout << "Enter value to add: ";
+        std::cin >> value;
+        stack.push(value);
+        std::cout << "Item added successfully." << std::endl;
+    }
+
+    static void editItem(Stack<T>& stack) {
+        if (stack.isEmpty()) {
+            std::cout << "Stack is empty. Cannot edit item." << std::endl;
+            return;
+        }
+
+        std::cout << "Enter new value: ";
+        T newValue;
+        std::cin >> newValue;
+
+        try {
+            stack.peek() = newValue;
+            std::cout << "Item edited successfully." << std::endl;
+        }
+        catch (const std::out_of_range& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+
+    static void displayItems(const Stack<T>& stack) {
+        if (stack.isEmpty()) {
+            std::cout << "Stack is empty." << std::endl;
+            return;
+        }
+
+        std::cout << "Items in the stack: " << stack << std::endl;
+    }
+
+    static void deleteItem(Stack<T>& stack) {
+        if (stack.isEmpty()) {
+            std::cout << "Stack is empty. Cannot delete item." << std::endl;
+            return;
+        }
+
+        try {
+            stack.pop();
+            std::cout << "Item deleted successfully." << std::endl;
+        }
+        catch (const std::out_of_range& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+    }
+
+    static void saveToFile(const Stack<T>& stack) {
+        std::ofstream outputFile("stack_data.txt");
+        if (!outputFile.is_open()) {
+            std::cerr << "Error opening file for writing." << std::endl;
+            return;
+        }
+
+        outputFile << stack;
+        std::cout << "Stack data saved to file." << std::endl;
+
+        outputFile.close();
+    }
+
+    static void loadFromFile(Stack<T>& stack) {
+        std::ifstream inputFile("stack_data.txt");
+        if (!inputFile.is_open()) {
+            std::cerr << "Error opening file for reading." << std::endl;
+            return;
+        }
+
+        inputFile >> stack;
+        std::cout << "Stack data loaded from file." << std::endl;
+
+        inputFile.close();
+    }
 };
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-
-    int tableSize;
-    std::cout << "Введите размер таблицы: ";
-    std::cin >> tableSize;
-
-    HashTable<std::string> myHashTable(tableSize);
-
-    while (true) {
-        // Выводим меню
-        std::cout << "\n===== Меню =====\n";
-        std::cout << "1. Добавить/редактировать элемент\n";
-        std::cout << "2. Удалить элемент\n";
-        std::cout << "3. Вывести все элементы\n";
-        std::cout << "4. Выйти\n";
-        std::cout << "Выберите действие: ";
-
-        int choice;
-        std::cin >> choice;
-
-        switch (choice) {
-        case 1: {
-            // Добавление/редактирование элемента
-            int key;
-            std::string value;
-
-            std::cout << "Введите ключ: ";
-            std::cin >> key;
-
-            std::cout << "Введите значение ";
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::getline(std::cin, value);
-
-            myHashTable.insert(key, value);
-
-            std::cout << "Элемент добавлен/отредактирован\n";
-            break;
-        }
-
-        case 2: {
-            // Удаление элемента
-            int key;
-            std::cout << "Введите ключ элемента для удаления: ";
-            std::cin >> key;
-
-            if (myHashTable.remove(key)) {
-                std::cout << "Элемент успешно удален\n";
-            }
-            else {
-                std::cout << "Элемент не найден\n";
-            }
-            break;
-        }
-
-        case 3: {
-            // Вывод всех элементов
-            myHashTable.printAllElements();
-            break;
-        }
-
-        case 4: {
-            // Выход из программы
-            return 0;
-        }
-
-        default:
-            std::cout << "Некорректный выбор, попробуйте снова\n";
-        }
-    }
+    Menu<int>::run();
 
     return 0;
-}
-
-template <typename T>
-HashTable<T>::HashTable() : tableSize(100), table(new HashNode<T>* [100]) {
-    for (int i = 0; i < tableSize; ++i) {
-        table[i] = nullptr;
-    }
-}
-
-template <typename T>
-HashTable<T>::HashTable(int size) : tableSize(size) {
-    if (size <= 0) {
-        std::cerr << "Некорректный размер таблицы. Используется значение по умолчанию (100).\n";
-        tableSize = 100;
-    }
-
-    table = new HashNode<T>*[tableSize];
-    for (int i = 0; i < tableSize; ++i) {
-        table[i] = nullptr;
-    }
-}
-
-template <typename T>
-HashTable<T>::~HashTable() {
-    for (int i = 0; i < tableSize; ++i) {
-        HashNode<T>* current = table[i];
-        while (current != nullptr) {
-            HashNode<T>* temp = current;
-            current = current->next;
-            delete temp;
-        }
-    }
-    delete[] table;
-}
-
-template <typename T>
-void HashTable<T>::printAllElements() const {
-    std::cout << "\nВсе элементы таблицы:\n";
-
-    for (int i = 0; i < tableSize; ++i) {
-        HashNode<T>* current = table[i];
-        while (current != nullptr) {
-            std::cout << "Ключ: " << current->key << ", Значение: " << current->value << "\n";
-            current = current->next;
-        }
-    }
-}
-
-template <typename T>
-int HashTable<T>::getTableSize() const {
-    return tableSize;
-}
-
-template <typename T>
-void HashTable<T>::insert(int key, const T& value) {
-    int index = hashFunction(key);
-    HashNode<T>* newNode = new HashNode<T>(key, value);
-    HashNode<T>* current = table[index];
-    HashNode<T>* prev = nullptr;
-
-    while (current != nullptr) {
-        if (current->key == key) {
-            if (prev == nullptr) {
-                table[index] = newNode;
-            }
-            else {
-                prev->next = newNode;
-            }
-            newNode->next = current->next;
-            delete current;
-            return;
-        }
-        prev = current;
-        current = current->next;
-    }
-
-    newNode->next = table[index];
-    table[index] = newNode;
-}
-
-template <typename T>
-bool HashTable<T>::remove(int key) {
-    int index = hashFunction(key);
-    HashNode<T>* current = table[index];
-    HashNode<T>* prev = nullptr;
-
-    while (current != nullptr) {
-        if (current->key == key) {
-            if (prev == nullptr) {
-                table[index] = current->next;
-            }
-            else {
-                prev->next = current->next;
-            }
-            delete current;
-            return true;
-        }
-        prev = current;
-        current = current->next;
-    }
-
-    return false;
-}
-
-template <typename T>
-int HashTable<T>::hashFunction(int key) const {
-    return key % tableSize;
 }
